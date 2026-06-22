b710f694eb78ca3a2aeac7761b89ce7d08bc1b51
zhangheyi <zhy_chihuoyimei@163.com>
2026-06-22 15:15:20 +0800

    更新 submit.sh 文件中的 scp 命令，简化远程路径以提高可读性

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,4 +1,4 @@
 # 下载远程服务器文件到本地的命令:
-# scp -r 4090:/mnt/sda/home/zhangheyi/projects/RWKV/eval.py .
+# scp -r 4090:/mnt .
 # scp [选项] 本地文件路径 用户名@服务器地址:远程目标路径
-# scp -r my_folder/ 4090:/mnt/sda/home/zhangheyi/projects/RefRWKV/
+# scp -r my_folder/ 4090:/mnt
